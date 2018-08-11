cead7e65eff47b75864283f870af6cf096183dab
Inder Kumar Rathore <contact.mr.rathore@gmail.com>
2018-08-11 14:02:11 +0530

    Intial xcode project

---
+++ NoStoryboardSample/NoStoryboardSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NoStoryboardSample
+//
+//  Created by Inder Kumar Rathore on 11/08/18.
+//  Copyright © 2018 Inder Kumar Rathore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
